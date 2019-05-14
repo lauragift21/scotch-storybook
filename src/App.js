5c0efb83846db96f5f6552c2f936e8c1c75081ed
Gift <laurandidi21@gmailcom>
2019-05-14 20:31:36 +0100

    Edit css for component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button'
+import Image from './components/Image'
 
 function App() {
   return (
     <div className="App">
       <Button />
+      <Image />
     </div>
   );
 }
--- src/stories/imageStories.js
+++ src/stories/imageStories.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+
 import Image from '../components/Image'
 
-storiesOf('Image', module).add('Image', () => <Image />)
+storiesOf('Image', module)
+  .add('Image', () => <Image />)
